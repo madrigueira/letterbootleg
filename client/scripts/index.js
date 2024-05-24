6b22e3f44c0fd2397e11a3b10e31ff6d3746189f
madrigueira <mndias29@gmail.com>
2024-05-24 15:31:44 -0300

    Cabeçalho pronto e responsivo

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,2 +1,8 @@
 // Linkando os components via load JQuery 
 $("#header").load("./components/header/index.html");
+
+// Abrir e fechar menu de navegação (Nav) no mobile
+function toggleNav(){
+  const nav = document.querySelector("nav")
+  nav.classList.toggle("active")
+}
