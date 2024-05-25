2162749b4177bd3b2945ff1716e30df204434bf5
madrigueira <mndias29@gmail.com>
2024-05-25 15:37:13 -0300

    Adicionado carousel Splide e Home responsiva

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -6,3 +6,22 @@ function toggleNav(){
   const nav = document.querySelector("nav")
   nav.classList.toggle("active")
 }
+
+var splide = new Splide( '.splide', {
+  perPage: 6,
+  perMove: 1,
+  pagination: false,
+  omitEnd: true,
+  gap: '10px',
+  rewind : true,
+  breakpoints: {
+		850: {
+			perPage: 4,
+		},
+    500: {
+			perPage: 3,
+		},
+  }
+} );
+
+splide.mount();
