6c3f0c27def56e0a43c49506a7cd4828ab442d56
madrigueira <mndias29@gmail.com>
2024-05-26 00:54:02 -0300

    Adicionando API do TMDB na Home

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -7,11 +7,11 @@ function toggleNav(){
   nav.classList.toggle("active")
 }
 
+// Carousel de filmes da Home 
 var splide = new Splide( '.splide', {
   perPage: 6,
   perMove: 1,
   pagination: false,
-  omitEnd: true,
   gap: '10px',
   rewind : true,
   breakpoints: {
@@ -25,3 +25,27 @@ var splide = new Splide( '.splide', {
 } );
 
 splide.mount();
+
+// Solicitação HTTP pra API do The Movie Data Base
+const optionsTMDB = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2Q4MzZiNTJlZmFiNzkxYWYxOWI3YWM0OTQxZTdjNyIsInN1YiI6IjYwNzdjZTExNzc3NmYwMDAyOTg1MDRhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ppFAJLYKfn3s0gzSs7lCxsmkH2de4y7B86xZGug2_8A'
+  }
+};
+
+// Fecth nos filmes mais recentes pela API do TMDB
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=pt-BR', optionsTMDB)
+  .then(response => response.json())
+  .then(response => {
+    let numberSlide = 1
+    response.results.slice(0, 12).forEach(movie => {
+      const movieDiv = document.createElement('div');
+      movieDiv.classList.add('poster');
+      movieDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/w780${movie.poster_path})`;
+      document.querySelector(`.slide${numberSlide}`).appendChild(movieDiv);
+      numberSlide++
+    });
+  })
+  .catch(err => console.error(err));
